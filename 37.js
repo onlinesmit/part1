20252b1b775e4f4ed49a055e474264194d4283b2
onlinesmit <onlinesmitg@gmail.com>
2023-11-03 23:29:32 +0530

    .

--- 37.js
+++ 37.js
@@ -4,10 +4,10 @@ const person = {
     name: "Smit",
     age: 22,
     "person hobbies": ["guitar", "sleeping", "listening music"]
-//in javascript not allow space if we want to write like (person hobbies) it is wrong instead of this we use in string this "person hobbies"
+//in javascript not allow space if we want to write like (person hobbies) it is wrong instead of this we use like string this "person hobbies"
 }
 // console.log(person);
 console.log(person["person hobbies"]);
-// we can use like this console.log(person.person hobbies); but the problem is java script not access space so here use string like method in bracket
-person["key"] = "harshitvashisth@gmail.com";
+// we can use like this console.log(person.person hobbies); but the problem is javascript not access space so here use string like method in bracket
+person["key"] = "skg@gmail.com";
 console.log(person);
